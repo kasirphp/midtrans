9022c6a701e685e723ae67341d042fb43c99f592
Risang Baskoro <risangbaskoro@gmail.com>
2023-11-16 20:32:47 +0700

    Reconfigure Rector

--- rector.php
+++ rector.php
@@ -1,23 +1,30 @@
 <?php
 
-declare(strict_types=1);
-
-require_once __DIR__ . '/vendor/autoload.php';
-
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+        __DIR__.'/src',
+        __DIR__.'/tests',
     ]);
 
     $rectorConfig->rules([
         DeclareStrictTypesRector::class,
         InlineConstructorDefaultToPropertyRector::class,
         FinalizeClassesWithoutChildrenRector::class,
+    ]);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION,
+        SetList::PRIVATIZATION,
     ]);
 };
